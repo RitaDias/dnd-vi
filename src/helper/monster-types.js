95e222772b9c175f83ad999dc8cf4c3a029b3d19
Rita Dias <anarita151@gmail.com>
2021-03-20 11:03:17 +0100

    refactor: Change colors for different monster types

--- src/helper/monster-types.js
+++ src/helper/monster-types.js
@@ -1,18 +1,18 @@
 const MONSTER_TYPES = {
   aberration: "#f5222d",
-  beast: "#fa8c16",
+  beast: "#FE5F55",
   celestial: "#fadb14",
-  construct: "#002329",
+  construct: "#5D5F71",
   dragon: "#faad14",
   elemental: "#13c2c2",
   fey: "#a0d911",
   fiend: "#fa541c",
-  giant: "#612500",
+  giant: "#AD8A64",
   humanoid: "#1890ff",
   monstrosity: "#eb2f96",
   ooze: "#2f54eb",
   plant: "#52c41a",
-  "swarm-of-tiny-beasts": "#262626",
+  "swarm-of-tiny-beasts": "#75daae",
   undead: "#722ed1"
 };
 
