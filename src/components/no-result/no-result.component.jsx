2b1a1e98b10c11477de66fc0a9a4f4601dc071e7
Rita Dias <anarita151@gmail.com>
2021-03-25 19:27:11 +0100

    style: Add credit

--- src/components/no-result/no-result.component.jsx
+++ src/components/no-result/no-result.component.jsx
@@ -2,6 +2,7 @@ import { ReactComponent as RawrIcon } from "../../Assets/rawr.svg";
 
 import "./no-result.styles.scss";
 
+// icon By Freepik
 const NoResult = () => (
   <div className="empty-slate">
     <RawrIcon className="icon" />
